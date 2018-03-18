ee82250fc463c2764c945167fccf2a6cad0b5980
claudineyjr <claudiney.alves@fatec.sp.gov.br>
2018-03-18 20:42:40 -0300

    Logic change in delete verb

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 var express = require('express');
 var app = express();
+var alunos = require('./alunos');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-var alunos = require('./alunos');
-
-
-app.listen(3000, (req,res) => {
-    console.log('server working');
+app.listen(3003, function(req, res) {
+    console.log('servidor funcionando');
 });
 
-app.get('/aluno/:id', (req, res) => {
-    let resposta = alunos.filter((aluno) => {
-        return aluno.id == req.params.id;
-    })
-    if(resposta.length < 1) {
-        res.statusCode = 204;
-        res.send();
-    }
-    res.send(resposta[0]);
-});
 
 app.get('/aluno', (req, res) => {
     res.send(alunos);
 });
 
+app.get('/aluno/:id', (req, res) => {
+    const resposta = alunos.filter((aluno) => {
+        return aluno.id == req.params.id;
+    });
+    if(resposta.length < 1) {
+        res.statusCode = 204;
+        res.send();
+        return;
+    }
+    res.send(resposta[0]);
+});
+
+app.delete('/aluno/:id', (req, res) => {
+    const id = req.params.id;
+    const aluno = alunos.filter((aluno, indice) => {
+        if(aluno.id == id) {
+            alunos.splice(indice, 1);
+        }
+        return true;
+    });
+    if(aluno.length < 1) {
+        res.statusCode = 204;
+        res.send();
+        return;
+    }
+    res.send('Aluno excluído com sucesso!');
+});
+
 app.post('/aluno', (req, res) => {
-    const body = req.body || {};
-    const newStudent = {
+    const body = req.body;
+    let newStudent = {
         id: alunos.length + 1,
         name: body.name,
         course: body.course,
         institution: body.institution
     };
     alunos.push(newStudent);
-    res.send('Aluno inserido com sucesso!');
-});
-
-app.delete('/aluno/:id', (req, res) => {
-    const id = req.params.id;
-    if(id > alunos.length) {
-        res.statusCode = 204;
-        res.send();
-        return;
-    }
-    alunos.splice(id-1, 1);
-    res.send('Aluno removido com sucesso!');
-});
-
+    res.send('Aluno cadastrado com sucesso!');
+})
