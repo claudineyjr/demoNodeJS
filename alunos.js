5874b4507bdbb3426a5878171d2ee1e3e3006242
claudineyjr <claudiney.alves@fatec.sp.gov.br>
2018-03-18 20:03:33 -0300

    A simple API REST working on an array

---
+++ alunos.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Claudiney",
+        course: "Análise e desenvolvimento de sistemas",
+        institution: "FATEC - Mogi das Cruzes"
+    },
+    {
+        id: 2,
+        name: "Alexandre",
+        course: "Gestão de TI",
+        institution: "FATEC - Itaquaquecetuba"
+    },
+    {
+        id: 3,
+        name: "Lucas",
+        course: "Análise e desenvolvimento de sistemas",
+        institution: "FATEC - Mogi das Cruzes"
+    },
+    {
+        id: 4,
+        name: "Gisele",
+        course: "Design Gráfico",
+        institution: "Faculdade de Tecnologia Carlos Drummond de Andrare"
+    },
+    {
+        id: 5,
+        name: "Morganna",
+        course: "Análise e desenvolvimento de sistemas",
+        institution: "FATEC - Mogi das Cruzes"
+    }
+]
---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+var alunos = require('./alunos');
+
+
+app.listen(3003, (req,res) => {
+    console.log('server working');
+});
+
+app.get('/aluno/:id', (req, res) => {
+    let resposta = alunos.filter((aluno) => {
+        return aluno.id == req.params.id;
+    })
+    if(resposta.length < 1) {
+        res.statusCode = 204;
+        res.send();
+    }
+    res.send(resposta[0]);
+});
+
+app.get('/aluno', (req, res) => {
+    res.send(alunos);
+});
+
+app.post('/aluno', (req, res) => {
+    const body = req.body || {};
+    const newStudent = {
+        id: alunos.length + 1,
+        name: body.name,
+        course: body.course,
+        institution: body.institution
+    };
+    alunos.push(newStudent);
+    res.send('Aluno inserido com sucesso!');
+});
+
+app.delete('/aluno/:id', (req, res) => {
+    const id = req.params.id;
+    if(id > alunos.length) {
+        res.statusCode = 204;
+        res.send();
+        return;
+    }
+    alunos.splice(id-1, 1);
+    res.send('Aluno removido com sucesso!');
+});
+
